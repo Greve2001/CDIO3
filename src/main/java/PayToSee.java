88b3203dba2a52e8d8f8cd93aea63cc09fc8fb5b
mikael <mikael.fangel@pm.me>
2021-11-08 19:56:35 +0100

    initial commit

---
+++ src/main/java/PayToSee.java
@@ -0,0 +1,12 @@
+public class PayToSee extends Square{
+    private final int amount;
+
+    public PayToSee(String name, int position, int amount) {
+        super(name, position);
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
