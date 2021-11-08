72e799ff093c72c03193ecf3e84c203a13159bc1
Emil Falck Hansen <falcken78@gmail.com>
2021-11-08 19:06:39 +0100

    Updated classes Account,Player and Token

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -5,16 +5,12 @@ public class Account {
         balance = 1000; // Default start amount
     }
 
-    public void updateBalance(int value) {
-        // Ensures balance never gets beneath 0.
-        if (balance + value <= 0)
-            balance = 0;
-        else
-            balance += value;
+    public Boolean updateBalance() {
+        boolean update = false;
+        return update = true;
     }
 
     public int getBalance(){
         return balance;
     }
-
 }
