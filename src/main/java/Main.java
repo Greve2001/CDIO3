956d3bbb4e0eac827103f0f63ed85bd9bd900659
Christian Andersen <s133288@dtu.dk>
2021-11-09 19:33:11 +0100

    Main burde have alle de methods den skal bruge. Dog er der flere afsnit der er kommenteret ud indtil videre.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        MonopolyJunior monopoly = new MonopolyJunior(4);
+        MonopolyJunior monopoly = new MonopolyJunior(4);//need to changeable with some user input later.
+        //MonopolyJunior.inputNames();//For name input later (might never go live)
         MonopolyJunior.giveStartMoney();
-        monopoly.playGame();
+        //MonopolyJunior.whoGoesFirst();//let all roll, and highest goes first
+        do {
+            //todo logik
+            monopoly.playGame();
+        }while(!win_condition);
+        //MonopolyJunior.decideAndAnnounceWinner();//to find and Announce the winner
     }
 }
