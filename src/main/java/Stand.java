45548fd2f32bf7d36fd7c237a255000a3a476a3f
Emil Falck Hansen <falcken78@gmail.com>
2021-11-09 19:30:34 +0100

    Updated Stand

--- src/main/java/Stand.java
+++ src/main/java/Stand.java
@@ -1,2 +1,9 @@
 public class Stand {
+    private int positionOnBoard;
+    private static int bodNR;
+    private boolean runOutOfTokens = false;
+    public Boolean assignToken () {
+        boolean assign = false;
+        return assign = true;
+    }
 }
