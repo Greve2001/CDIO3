579f6896cbb7442bedef45a9eb88f913920b1224
Pi Emilie <s113609@dtu.dk>
2021-11-12 14:37:48 +0100

    new default constructor for Deck-class, with initialization of an array of ChanceCard objects. Addition of pullCard-method and shuffleCard-method, logic to come.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,6 +1,29 @@
 public class Deck {
 
-    public Deck(int deckSize){
+    private ChanceCard[] chanceCardDeck;
+    private int drawCardCount = 0;
 
+    public Deck(int deckSize){
+       this.chanceCardDeck = new ChanceCard[deckSize];
     }
+
+    public Deck(){
+        this.chanceCardDeck = new ChanceCard[3];
+
+        chanceCardDeck[0] = new ChanceCard();
+        chanceCardDeck[1] = new ChanceCard();
+        chanceCardDeck[2] = new ChanceCard();
+
+        shuffleDeck();
+    }
+
+
+    private void shuffleDeck(){
+    }
+
+    public ChanceCard pullCard(){
+        // for now hardcoded
+        return new ChanceCard();
+    }
+
 }
