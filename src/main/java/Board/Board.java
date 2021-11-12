cf0a062fea65a72fbe122c2e4d0f7e7f013e059f
mikael <mikael.fangel@pm.me>
2021-11-12 08:33:12 +0100

    added comments about how the class works

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -11,6 +11,7 @@ public class Board {
     public Board (){
         readCSV("board.csv");
 
+        // initialises the objects based on the hashmap
         for (int i = 0; i < mapList.toArray().length; i++) {
             switch (mapList.get(i).get("type")) {
 
@@ -19,22 +20,27 @@ public class Board {
 
     }
 
+    // Reads a CSV file an stores the result as a list of hashmaps
     public void readCSV(String file) {
+        // Gets the class loader and reads the file from the ressources folder
         ClassLoader classLoader = Board.class.getClassLoader();
         InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(classLoader.getResourceAsStream(file)));
 
+        // Creates a scanner that reads the inputstream
         Scanner scan = new Scanner(reader);
         Scanner lineReader;
 
         int index = 0;
         int line = 0;
 
+        // Scans one line at a time and then breaks up the line using the delimiter ","
         while(scan.hasNextLine()) {
             lineReader = new Scanner(scan.nextLine());
             lineReader.useDelimiter(",");
 
             mapList.add(new HashMap<String, String>());
 
+            // puts the values to the hashmap
             while(lineReader.hasNext()) {
                 switch (index) {
                     case 0 : mapList.get(line).put("pos", lineReader.next());         break;
