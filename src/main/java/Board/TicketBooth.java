a85ae0beb6a3d3470b2baf4cb4e406b62f85301d
mikael <mikael.fangel@pm.me>
2021-11-12 08:27:42 +0100

    moved to board package

--- src/main/java/Board/TicketBooth.java
+++ src/main/java/Board/TicketBooth.java
@@ -1,8 +1,11 @@
-public class Stand {
+package Board;
+
+public class TicketBooth {
     private int positionOnBoard;
     private static int bodNR;
     private boolean runOutOfTokens = false;
-    public Boolean assignToken () {
+
+    public Boolean assignToken() {
         boolean assign = false;
         return assign = true;
     }
