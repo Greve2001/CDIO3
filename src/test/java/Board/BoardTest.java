1a6e2e30cc453152ba411bc5dde71afd3845af48
mikael <mikael.fangel@pm.me>
2021-11-13 13:22:20 +0100

    added test for subclass type

--- src/test/java/Board/BoardTest.java
+++ src/test/java/Board/BoardTest.java
@@ -1,10 +1,25 @@
 package Board;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    void GetSubClass() {
+        String result = board.getSquare(1).getClass().getSimpleName();
+        String expected = "Go";
+
+        assertEquals(expected, result);
+    }
 
     @Test
     void readCSV() {
