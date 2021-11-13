3a12583d1427a9f5c4f83c34393c3fa9d584e272
mikael <mikael.fangel@pm.me>
2021-11-13 14:44:54 +0100

    added boothOwners to amusements

--- src/main/java/Board/Amusement.java
+++ src/main/java/Board/Amusement.java
@@ -1,8 +1,11 @@
 package Board;
 
+import MonopolyJunior.Player;
+
 public class Amusement extends Square{
     private final String color;
     private final int price;
+    private Player boothOwner;
 
     public Amusement(String name, int position, String color, int price) {
         super(name, position);
@@ -17,5 +20,13 @@ public class Amusement extends Square{
 
     public String getColor() {
         return color;
+    }
+
+    public void setBoothOwner(Player boothOwner) {
+        this.boothOwner = boothOwner;
+    }
+
+    public Player getBoothOwner() {
+        return boothOwner;
     }
 }
