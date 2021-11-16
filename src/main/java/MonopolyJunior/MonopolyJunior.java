2a3bf3d56940a67801ef499d73a3fbe2eac1401f
Christian Andersen <s133288@dtu.dk>
2021-11-16 13:50:41 +0100

    removed not used brackets

--- src/main/java/MonopolyJunior/MonopolyJunior.java
+++ src/main/java/MonopolyJunior/MonopolyJunior.java
@@ -82,9 +82,8 @@ public class MonopolyJunior {
                     }
                 }
                 else{
-                    if(board.hasMonopoly(position)) {
+                    if(board.hasMonopoly(position))
                         pay(amusement.getBoothOwner(), amusement.getPrice() * 2);
-                    }
                     else
                         pay(amusement.getBoothOwner(), amusement.getPrice());
                 }
@@ -199,6 +198,6 @@ public class MonopolyJunior {
     }
 
     public void decideAndAnnounceWinner(){
-        
+
     }
 }
