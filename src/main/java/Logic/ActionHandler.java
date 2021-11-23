0a2958c37bd306461f5eb4f9b9c39bd35121e65d
Frederik Greve Petersen <frederik.greve@gmail.com>
2021-11-23 21:40:52 +0100

    Added GUI display of Pennybag being emptied

--- src/main/java/Logic/ActionHandler.java
+++ src/main/java/Logic/ActionHandler.java
@@ -95,6 +95,7 @@ public class ActionHandler {
             case "PennyBag":
                 // Get all the money saved up
                 currentPlayer.updateBalance(pennyBag.withDraw());
+                GUIController2.setPennyBagValue(pennyBag.getAmountOfMoneyPlaced(), );
                 break;
 
             case "PayToSee":
