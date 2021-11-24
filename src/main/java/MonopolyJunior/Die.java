221b81d3022bde5b388e190c0ef09cbdbc4493ee
Christian Andersen <s133288@dtu.dk>
2021-11-24 21:14:27 +0100

    made nr of sides final and local

--- src/main/java/MonopolyJunior/Die.java
+++ src/main/java/MonopolyJunior/Die.java
@@ -2,13 +2,13 @@ package MonopolyJunior;
 
 public class Die {
     private int faceValue;
+    final int NROFSIDES = 6;
 
     public Die() {
         roll();
     }
 
     public void roll() {
-        final int NROFSIDES = 6;
         faceValue = (int) (Math.random() * NROFSIDES) + 1;
     }
 
